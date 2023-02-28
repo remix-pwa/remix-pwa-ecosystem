4b83600179f35947562d35abaf30c21c8a31ed0c
Abdur-Rahman <abbadshaf05@gmail.com>
2023-02-28 16:35:12 +0100

    initialized `client` apis

---
+++ crates/client/src/client/mod.rs
@@ -0,0 +1,10 @@
+pub mod response;
+
+pub mod clipboard;
+pub mod connection;
+pub mod fullscreen;
+pub mod visibility;
+pub mod permissions;
+pub mod language;
+pub mod langauges;
+pub mod geolocation;
